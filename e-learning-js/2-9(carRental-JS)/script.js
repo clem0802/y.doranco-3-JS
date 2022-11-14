46566ccb1920442209569943bcbfec77ca3d6da3
clem0802 <hhcc0802@gmail.com>
2022-11-14 20:09:16 +0100

    --

--- e-learning-js/2-9(carRental-JS)/script.js
+++ e-learning-js/2-9(carRental-JS)/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-    $(".img_container img:first-child").addClass("active");
+    // $(".img_container img:first-child").addClass("active");
 
     $("#burger_menu").click(function(){
         $("#scrolling_menu").slideToggle();
         $("#burger_menu").toggleClass("bg-color-white");
-        $("#burger_menu div").toggleClass("bu-color-black");
+        $("#burger_menu div").toggleClass("bg-color-black");
     });
 
+
+    // ci-dessous, pr regarder si le scroll effectué vers le bas 
+    // + la taille de la partie visible de notre fenêtre
+    // égalent bien à la taille de la page
     $(window).scroll(function(){
-        if($(window).scrollTop() + $(window).height() == $(document).height()){
+        if($(window).scrollTop() + $(window).height() === $(document).height()){
+            console.log("Bottom!!");
+            // ça va durer 2 secondes
             $(".toBeFaded").fadeIn(2000);
         }
     });
@@ -19,9 +25,20 @@ $(document).ready(function(){
 
 
     function init(elem){
+        // Counter, faut savoir la position de l'img active
         counter = elem.parent().find(".img_container img.active").index();
+        // PositionLeft de img_container
         leftValue = counter * -100;
+        // Number of pictures
         nbrImgs = elem.parent().find(".img_container img").length;
+
+        // if(counter < nbrImgs - 1){
+        //     $(this).parent().find(".img_container img").removeClass("active");
+        //     $(this).parent().find(".img_container img").eq(counter - 1).addClass("active");
+        //     leftValue += 100;
+        //     $(this).parent().find(".img_container img").animate({left : leftValue + "%"}, 1000);
+        //     // counter++;
+        // }
     }
 
     function handleCarousel(scenario, elem){
