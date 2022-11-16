97a9f3092d2d20b6ffccdfef421c47231077eab4
clem0802 <hhcc0802@gmail.com>
2022-11-16 12:23:34 +0100

    --

--- e-learning-js/2-7(form-control-JS)/script.js
+++ e-learning-js/2-7(form-control-JS)/script.js
@@ -1,9 +1,8 @@
-
 function deactivateToolTips(){
     const spans = document.getElementsByTagName("span");
     const spansLength = spans.length;
 
-    for (let i=0; i<spansLength; i++){
+    for (let i = 0; i < spansLength; i++){
         if (spans[i].className == "tooltip"){
             spans[i].style.display = "none";
         }
@@ -12,7 +11,7 @@ function deactivateToolTips(){
 
 function getToolTip(elemHTML){
     while (elemHTML = elemHTML.nextSibling){
-        if (elemHTML.className = "tooltip"){
+        if (elemHTML.className == "tooltip") {
             return elemHTML;
         }
     }
@@ -20,24 +19,25 @@ function getToolTip(elemHTML){
 }
 
 
-// --------------------------
+
+
 let check = {};
 
-// sex
-check["sex"] = function(){
+// Sex
+check["sex"] = function (){
     console.log("user sex checked");
 
     let sex = document.getElementsByName("sex"),
     tooltip = getToolTip(sex[1].parentNode);
 
-    // if(sex[0].checked || sex[1].checked || sex[2].checked){
-    if(sex[0].checked || sex[1].checked){
+    if(sex[0].checked || sex[1].checked) {
         tooltip.style.display = "none";
         return true;
     } else{
         tooltip.style.display = "inline-block";
         return false;
     }
+
 };
 
 // lastName
@@ -45,13 +45,13 @@ check["lastName"] = function(id){
     console.log("user lastName checked");
 
     let elemHTML = document.getElementById(id),
-    tooltip = getToolTip(elemHTML);
+        tooltip = getToolTip(elemHTML);
 
-    if(elemHTML.value.length >= 2){
+    if (elemHTML.value.length >= 2) {
         elemHTML.className = "correct";
         tooltip.style.display = "none";
         return true;
-    } else{
+    } else {
         elemHTML.className = "incorrect";
         tooltip.style.display = "inline-block";
         return false;
@@ -61,54 +61,54 @@ check["lastName"] = function(id){
 // firstName
 check["firstName"] = check["lastName"];
 
-// login
+// age
+check["age"] = function(){
+    console.log("user age checked");
+
+    let age = document.getElementById("age"),
+        tooltip = getToolTip(age),
+        ageValue = parseInt(age.value);
+
+    if (ageValue >= 13 && ageValue <= 120) {
+        age.className = "correct";
+        tooltip.style.display = "none";
+        return true;
+    } else {
+        age.className = "incorrect";
+        tooltip.style.display = "inline-block";
+        return false;
+    }
+};
+
+// login-user
 check["login"] = function(){
     console.log("user login checked");
-    let login = document.getElementById("login");
-    tooltip = getToolTip(login);
+    let login = document.getElementById("login"),
+        tooltip = getToolTip(login);
 
-    if(login.value.length >= 4){
+    if (login.value.length >= 4) {
         login.className = "correct";
         tooltip.style.display = "none";
         return true;
-    } else{
+    } else {
         login.className = "incorrect";
         tooltip.style.display = "inline-block";
         return false;
     }
 };
 
-// age
-check["age"] = function(){
-    console.log("user age checked");
-
-    let age = document.getElementById("age"),
-    tooltip = getToolTip(age),
-    ageValue = parseInt(age.value);
-
-    if(ageValue >= 13 && ageValue <= 120){
-        age.className = "correct";
-        tooltip.style.display = "none";
-        return true;
-    } else{
-        age.calssName = "incorrect";
-        tooltip.style.display = "incorrect";
-        return false;
-    }
-};
-
 // pwd1
 check["pwd1"] = function(){
-    console.log("user password checked");
+    console.log("user pwd1 checked");
 
     let pwd1 = document.getElementById("pwd1"),
-    tooltip = getToolTip(pwd1);
+        tooltip = getToolTip(pwd1);
 
-    if(pwd1.value.length >= 6){
+    if (pwd1.value.length >= 6) {
         pwd1.className = "correct";
         tooltip.style.display = "none";
         return true;
-    } else{
+    } else {
         pwd1.className = "incorrect";
         tooltip.style.display = "inline-block";
         return false;
@@ -117,20 +117,22 @@ check["pwd1"] = function(){
 
 // pwd2
 check["pwd2"] = function(){
-    console.log("user password confirmation checked");
-    let pwd1 = document.getElementById("pwd1");
-        pwd2 = document.getElementById("pwd2");
+    console.log("user pwd2 checked");
+
+    let pwd1 = document.getElementById("pwd1"),
+        pwd2 = document.getElementById("pwd2"),
         tooltip = getToolTip(pwd2);
 
-        if(pwd1.value == pwd2.value && pwd2.value != ""){
-            pwd2.className = "correct";
-            tooltip.style.display = "none";
-            return true;
-        } else{
-            pwd2.className = "incorrect";
-            tooltip.style.display = "inline-block";
-            return false;
-        }
+    if (pwd1.value == pwd2.value && pwd2.value != "") {
+        pwd2.className = "correct";
+        tooltip.style.display = "none";
+        return true;
+    } else {
+        pwd2.className = "incorrect";
+        tooltip.style.display = "inline-block";
+        return false;
+    }
+
 };
 
 // country
@@ -140,19 +142,17 @@ check["country"] = function(){
     let country = document.getElementById("country"),
     tooltip = getToolTip(country);
 
-    if(country.value !== "none"){
+    if(country.value !== "none") {
         tooltip.style.display = "none";
         return true;
     } else{
         tooltip.style.display = "inline-block";
         return false;
     }
-}
+};
 
-
-// --------------------------
-// IIFE
-(function(){
+//IIFE
+(function () {
 
     // form
     const myForm = document.getElementById("myForm");
@@ -160,10 +160,11 @@ check["country"] = function(){
     const listInputsLength = listInputs.length;
 
     // onKeyUp
-    for(let i=0; i<listInputsLength; i++){
-        if(listInputs[i].type == "text" || listInputs[i].type == "password"){
-            listInputs[i].onkeyup = function(){
-                check[this.id](this.id); // check.country("country")
+    for (let i = 0; i < listInputsLength; i++) {
+
+        if (listInputs[i].type == "text" || listInputs[i].type == "password") {
+            listInputs[i].onkeyup = function () {
+                check[this.id](this.id); //check.country("country")
             }
         }
     }
@@ -173,20 +174,20 @@ check["country"] = function(){
         e.preventDefault();
         let result = true;
 
-        for(let i in check){
+        for(let i in check) {
             result = check[i](i) && result;
         }
 
-        if(result){
+        if(result) {
             alert("Data OK");
-        } else{
-            alert("Wrong Data!!");
+        } else {
+            alert("Wrong Data");
         }
     }
 
     // reset
     myForm.onreset = function(e){
-        for(let i=0; i<listInputs.length; i++){
+        for(let i = 0; i < listInputs.length; i++) {
             listInputs[i].className = "";
         }
         deactivateToolTips();
