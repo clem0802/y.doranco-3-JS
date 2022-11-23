23102f6a86bfe9b29fb8d5dc2eefff1d465fcb0b
clem0802 <hhcc0802@gmail.com>
2022-11-23 14:02:44 +0100

    --

---
+++ exo-clem/exo8 (nov2022)/4-func.js
@@ -0,0 +1,56 @@
+// function find_max(nums) {
+//     let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+//     for (let num of nums) {
+//         if (num > max_num) {
+//         // (Fill in the missing line here)
+//                 max = max_num (??)
+//         }
+//     }
+//  return max_num;
+// }
+
+// ------------------------------------------
+function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
+makeShoppingList();
+// Remember to buy milk
+// Remember to buy bread
+// Remember to buy eggs
+
+// ------------------------------------------
+function rectangleArea(width, height) {
+  let area = width * height;
+  return area;
+}
+console.log(rectangleArea(5, 7)) 
+// undefined (without return)
+// 35 (with return)
+
+// ------------------------------------------
+function monitorCount(rows, columns){
+  return rows * columns;
+}
+const numOfMonitors = monitorCount(5,4);
+console.log(numOfMonitors); // 20
+
+// ------------------------------------------Celsius & Fahrenheit
+function getFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+};
+console.log(getFahrenheit(15)); // 59  (OR)
+
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+let fahrenheit = getFahrenheit(15);
+console.log(fahrenheit); // 59 (more complicated)
+
+// ------------------------------------------
+// ------------------------------------------
+// ------------------------------------------
